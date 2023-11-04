827bc872731657686eeb62482f1ca26121df24e1
Ayush1234567434324 <ayushbhandari3031@gmail.com>
2023-11-04 13:57:42 +0530

    commit changes

--- src/component/product/product-grid/product-grid.js
+++ src/component/product/product-grid/product-grid.js
@@ -4,11 +4,11 @@ import './product-grid.css';
 
 export default function ProductGrid(props) {
   const [products, setProducts] = useState([]);
-  const infoValue = props.info;
+  const infoValue = props.info; // Store props.info in a constant variable
 
   useEffect(() => {
     if (infoValue) {
-      const mappedProducts = infoValue.map(item => ({
+      const mappedProducts = infoValue.map((item) => ({
         id: item._id,
         title: item.title,
         thumbnail: item.thumbnail,
@@ -21,34 +21,48 @@ export default function ProductGrid(props) {
         feedBackTitle: item.feedBackTitle,
         pdf: item.pdf,
         images: item.images,
-        bullets:item.bullets
+        bullets: item.bullets,
       }));
       setProducts(mappedProducts);
     }
   }, [infoValue]);
 
-  const numColumns = window.innerWidth >= 768 ? 3 : 2;
-
-  const itemsPerPage = 6; // Number of products to display per page
   const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 6; // Adjust this number to control how many products are shown on each page.
 
-  const handlePageChange = (page) => {
-    setCurrentPage(page);
-  };
-
-  // Calculate the number of total pages
-  const totalPages = Math.ceil(products.length / itemsPerPage);
-
-  // Get the current products to display on the current page
   const startIndex = (currentPage - 1) * itemsPerPage;
   const endIndex = startIndex + itemsPerPage;
-  const productsToDisplay = products.slice(startIndex, endIndex);
+  const visibleProducts = products.slice(startIndex, endIndex);
+
+  const handlePageChange = (newPage) => {
+    setCurrentPage(newPage);
+  };
+
+  const totalPages = Math.ceil(products.length / itemsPerPage);
+
+  const paginationItems = [];
+  for (let i = 1; i <= totalPages; i++) {
+    paginationItems.push(
+      <li
+        key={i}
+        className={`page-item ${currentPage === i ? 'active' : ''}`}
+      >
+        <a
+          className="page-link"
+          href="#"
+          onClick={() => handlePageChange(i)}
+        >
+          {i}
+        </a>
+      </li>
+    );
+  }
 
   return (
     <div className="container">
       <div className="row">
-        {productsToDisplay.map((product) => (
-          <div className={`col-${12 / numColumns}`} key={product.id}>
+        {visibleProducts.map((product) => (
+          <div className="col-6 col-sm-4" key={product.id}>
             <Productcard
               artwork={product.thumbnail}
               info={product}
@@ -60,36 +74,26 @@ export default function ProductGrid(props) {
       </div>
 
       <div className="mt-5" style={{ display: "flex", justifyContent: "center" }}>
-        <nav aria-label="Pagination">
+        <nav aria-label="...">
           <ul className="pagination">
             <li className={`page-item ${currentPage === 1 ? 'disabled' : ''}`}>
-              <button
+              <a
                 className="page-link"
+                href="#"
                 onClick={() => handlePageChange(currentPage - 1)}
               >
                 Previous
-              </button>
+              </a>
             </li>
-            {Array.from({ length: totalPages }, (_, index) => (
-              <li
-                key={index}
-                className={`page-item ${currentPage === index + 1 ? 'active' : ''}`}
-              >
-                <button
-                  className="page-link"
-                  onClick={() => handlePageChange(index + 1)}
-                >
-                  {index + 1}
-                </button>
-              </li>
-            ))}
+            {paginationItems}
             <li className={`page-item ${currentPage === totalPages ? 'disabled' : ''}`}>
-              <button
+              <a
                 className="page-link"
+                href="#"
                 onClick={() => handlePageChange(currentPage + 1)}
               >
                 Next
-              </button>
+              </a>
             </li>
           </ul>
         </nav>
